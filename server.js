6777320ad0894477ec073e2011ef953cbe22ec84
akshinde6 <ashinde6@uncc.edu>
2023-05-12 08:32:21 -0400

    backend creation

--- server.js
+++ server.js
@@ -1,23 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
+app.use(express.static('backend'));
 
-app.set('view engine', 'ejs');
-app.set('views','frontend');
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/', function(req, res){
-    res.render('index');
-})
-
-app.get('/getBirthdayAge', (req, res)=>{
-    //since the round off value will return max 99, have added +1 to reach to 100
-    const randomNumber = Math.floor(Math. random() * 100)+1;
-    res.send(randomNumber.toString());
-})
-
-app.listen(3000, function(){
-    console.log('server started on port 3000');
-})
+// Start the API server
+require('./backend/app');
